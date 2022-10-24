96c07f38fc538709d496544fd135cd8a88d31578
BradleyParkerDev <bradleyscottparker@outlook.com>
2022-10-24 14:30:25 -0500

    hi

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+//Changes to paragraph
 let paragraph = document.querySelector('p');
 paragraph.style.color = "#add8e6";
+paragraph.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
 
+//Changes to heading
 let heading = document.querySelector('h1');
 heading.style.fontSize = "48px";
+
+//Changes to list items
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = "0.5";
+let item3 = document.querySelector('#item-3');
+item3.innerText = 'I say, "Hi!"';
